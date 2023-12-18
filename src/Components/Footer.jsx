78f7b7766e631f7417a3a3487017563ba2032229
Choudhary Saklen <sakoochoudhary@gmail.com>
2023-12-18 13:53:21 +0530

    setup web app

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer>
+    <div>
+        <h1>Choudhary Saklen</h1>
+        <p>@all right reserved</p>
+    </div>
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://www.youtube.com/@choudharysaklen9475" target={"_blank"}>Youtube</a>
+            <a href="https://www.linkedin.com/in/choudhary-saklen-812709228/?originalSubdomain=in" target={"_blank"}>Linkdin</a>
+            <a href="https://github.com/saklenkc" target={"_blank"}>Github</a>
+        </div>
+    </div>
+
+
+   </footer>
+  )
+}
+
+export default Footer
